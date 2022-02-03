178e9a85c61a3e8794a1d546cab777314b6240e6
joshua360x <jkw5499@psu.edu>
2022-02-03 15:00:12 -0500

    fixed list rendering for speakers

--- src/SpeakerList.js
+++ src/SpeakerList.js
@@ -1,15 +1,10 @@
 import React from 'react';
+import Speaker from './Speaker';
 
 export default function SpeakerList({ speakers }) {
-  return <div className='speakers'>
-    {
-      speakers.map((speaker, i) => <div className='speaker' key={speaker.BRAND + i}>
-        <p>{speaker.BRAND}</p>
-        <p>${speaker.PRICE}</p>
-        <p>{speaker.AMAZON_RATING} / 5</p>
-        <p>{speaker.HOW_LOUD} / 5</p>
-      </div>)
-    }
-  </div>;
+  return (
+    <div className="speakers">
+      <Speaker speakers={speakers} />
+    </div>
+  );
 }
-
